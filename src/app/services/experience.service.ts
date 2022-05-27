7dc0e1567eef0bd1e689e78917aac1fbb4bb4e27
Andres Camacho <andrelomartino@gmail.com>
2022-05-27 14:54:15 -0300

    entrega

---
+++ src/app/services/experience.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Experience } from '../models/experience';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExperienceService {
+  private apiServerUrl = '*';
+
+  constructor(private http: HttpClient) {}
+
+  public getExperience(): Observable<Experience[]> {
+    return this.http.get<Experience[]>(`${this.apiServerUrl}/api/experience`);
+  }
+  public addExperience(education: Experience): Observable<Experience> {
+    return this.http.post<Experience>(
+      `${this.apiServerUrl}/api/experience`,
+      education
+    );
+  }
+  public updateExperience(education: Experience): Observable<Experience> {
+    return this.http.put<Experience>(
+      `${this.apiServerUrl}/api/experience`,
+      education
+    );
+  }
+  public deleteExperience(experienceId: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiServerUrl}/api/experience/${experienceId}`
+    );
+  }
+}
